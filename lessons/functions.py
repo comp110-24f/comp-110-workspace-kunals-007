3047cb22555501ebf0c62bb5474f0860fd5cb065
kboss-007 <singh24k@ncssm.edu>
2024-08-27 19:25:48 -0400

    Finished Exercise 00!

---
+++ lessons/functions.py
@@ -0,0 +1,13 @@
+"""Practice with functions."""
+
+#from random import randint
+
+#print(randint(3, 17))
+
+#Function Definition
+def sum(num1: int, num2: int) -> int:
+    '''return sum of num1 and num2'''
+    return num1 + num2
+
+#Function Call
+print(sum(num1=2, num2=13))
